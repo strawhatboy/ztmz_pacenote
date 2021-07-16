efc2848fd93fcb2f08e16c446a5fa7ef23096786
strawhatboy <strawhat2010@live.cn>
2021-07-16 13:28:50 +0800

    fixed the file locking issue when replaying

---
+++ ZTMZ.PacenoteTool/AutoResampledCachedSound.cs
@@ -0,0 +1,38 @@
+﻿using NAudio.Extras;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTMZ.PacenoteTool
+{
+    public class AutoResampledCachedSound
+    {
+        public float[] AudioData { get; }
+        public WaveFormat WaveFormat { get; }
+
+        public AutoResampledCachedSound(string audioFileName)
+        {
+            using (var audioFileReader = new AudioFileReader(audioFileName))
+            {
+                var resampledAudio = new WaveToSampleProvider(new MediaFoundationResampler(
+                    new SampleToWaveProvider(audioFileReader),
+                    WaveFormat.CreateIeeeFloatWaveFormat(44100, 2)));
+                this.WaveFormat = resampledAudio.WaveFormat;
+
+                var wholeFile = new List<float>((int)(audioFileReader.Length / 4));
+                var buffer = new float[resampledAudio.WaveFormat.SampleRate * resampledAudio.WaveFormat.Channels];
+                int samplesRead;
+                while ((samplesRead = resampledAudio.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    wholeFile.AddRange(buffer.Take(samplesRead));
+                }
+                this.AudioData = wholeFile.ToArray();
+            }
+
+        }
+    }
+}
---
+++ ZTMZ.PacenoteTool/ZTMZAudioPlaybackEngine.cs
@@ -0,0 +1,58 @@
+﻿using NAudio.Extras;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+
+namespace ZTMZ.PacenoteTool
+{
+    public class ZTMZAudioPlaybackEngine : IDisposable
+    {
+        private readonly WaveOutEvent outputDevice;
+        private readonly MixingSampleProvider mixer;
+
+        public ZTMZAudioPlaybackEngine(int deviceID = -1, int desiredLatency = 50, int sampleRate = 44100, int channelCount = 2)
+        {
+            outputDevice = new WaveOutEvent();
+            mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channelCount));
+            mixer.ReadFully = true;
+            outputDevice.DeviceNumber = deviceID;
+            outputDevice.DesiredLatency = desiredLatency;
+            outputDevice.Init(mixer);
+            outputDevice.Play();
+        }
+
+        public void PlaySound(string fileName)
+        {
+            var input = new AudioFileReader(fileName);
+            AddMixerInput(new AutoDisposeFileReader(input));
+        }
+
+        private ISampleProvider ConvertToRightChannelCount(ISampleProvider input)
+        {
+            if (input.WaveFormat.Channels == mixer.WaveFormat.Channels)
+            {
+                return input;
+            }
+            if (input.WaveFormat.Channels == 1 && mixer.WaveFormat.Channels == 2)
+            {
+                return new MonoToStereoSampleProvider(input);
+            }
+            throw new NotImplementedException("Not yet implemented this channel count conversion");
+        }
+
+        public void PlaySound(AutoResampledCachedSound sound)
+        {
+            AddMixerInput(new AutoResampledCachedSoundSampleProvider(sound));
+        }
+
+        private void AddMixerInput(ISampleProvider input)
+        {
+            mixer.AddMixerInput(ConvertToRightChannelCount(input));
+        }
+
+        public void Dispose()
+        {
+            outputDevice.Dispose();
+        }
+    }
+}
