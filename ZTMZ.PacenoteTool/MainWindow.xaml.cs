a6554784a0d664795ecf5f619421aec49ed8d3a7
strawhatboy <strawhat2010@live.cn>
2021-10-02 17:31:06 +0800

    2.3.1: release ok

--- ZTMZ.PacenoteTool/MainWindow.xaml.cs
+++ ZTMZ.PacenoteTool/MainWindow.xaml.cs
@@ -442,7 +442,7 @@ WindowsAPICodePack-Shell (https://github.com/aybe/Windows-API-Code-Pack-1.1)
 AutoUpdater.NET (https://github.com/ravibpatel/AutoUpdater.NET)
 
 最后再次感谢ZTMZ Club组委会和群里大佬们的帮助与支持。
-", "关于本工具 v2.3", MessageBoxButton.OK, MessageBoxImage.Information);
+", "关于本工具 v2.3.1", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void btn_currentTrack_script_Click(object sender, RoutedEventArgs e)
